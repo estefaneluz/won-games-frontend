ab32f195784b0f7f753d3adb39d3a2f8d270bab6
estefaneluz <estefanecortes@gmail.com>
2023-05-01 23:21:34 -0300

    WIP: criando componente menu

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Menu from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+type Story = StoryObj<typeof Menu>
+
+export const Default: Story = {}
