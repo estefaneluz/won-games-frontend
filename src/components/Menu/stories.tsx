5e37490b8b36a961875fbe658b91ed61e75ca7ac
estefaneluz <estefanecortes@gmail.com>
2023-05-02 00:11:14 -0300

    WIP: estilizando o menu no mobile

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -3,7 +3,13 @@ import Menu from '.'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen'
+  },
+  background: {
+    default: 'dark'
+  }
 } as Meta
 
 type Story = StoryObj<typeof Menu>
