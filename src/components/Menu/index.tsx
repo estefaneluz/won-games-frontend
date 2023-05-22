588aa12a67cdc7accef16e99f3382ff7360f5e18
estefaneluz <estefanecortes@gmail.com>
2023-05-22 11:25:19 -0300

    INT: ajustando menu para desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,11 +5,12 @@ import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from '../../components/Logo'
+import MediaMatch from '../MediaMatch'
 import * as S from './styles'
 import Button from '../Button'
 
 type MenuProps = {
-  username: string
+  username?: string
 }
 
 const Menu = ({ username }: MenuProps) => {
@@ -17,12 +18,23 @@ const Menu = ({ username }: MenuProps) => {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon aria-label="Open menu" onClick={() => setIsOpen(true)} />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper>
+          <MenuIcon aria-label="Open menu" onClick={() => setIsOpen(true)} />
+        </S.IconWrapper>
+      </MediaMatch>
+
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
       <S.MenuGroup>
         <S.IconWrapper>
           <SearchIcon aria-label="Search" />
@@ -30,6 +42,11 @@ const Menu = ({ username }: MenuProps) => {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open shopping cart" />
         </S.IconWrapper>
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
